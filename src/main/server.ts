1f7a74bdb05c3fd90232ffa1310b2b48b868aea4
Matheus Nunes <mathieusnunes@gmail.com>
2020-09-16 19:24:42 -0300

    feat: ensure connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,10 @@
-import app from './config/app'
 
-app.listen(5050, () => console.log('server running at http://local:5050'))
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
