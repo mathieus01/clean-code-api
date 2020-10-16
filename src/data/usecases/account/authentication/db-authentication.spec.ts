737634fc43a59a23e51a323003a127a2870730e9
Matheus Nunes <mathieusnunes@gmail.com>
2020-10-16 08:39:55 -0300

    refactor: change Promises to use shorter version

--- src/data/usecases/account/authentication/db-authentication.spec.ts
+++ src/data/usecases/account/authentication/db-authentication.spec.ts
@@ -64,7 +64,7 @@ describe('DBAuthentication UseCase', () => {
   })
   test('Should return null if HashComparer returns false', async () => {
     const { sut, hashComparerStub } = makeSut()
-    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(Promise.resolve(false))
     const accessToken = await sut.auth(mockAuthentication())
     expect(accessToken).toBeNull()
   })
--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -60,7 +60,7 @@ describe('LoadSurveys Controller', () => {
   })
   test('Should return 204 if LoadSurvey returns empty', async () => {
     const { sut, loadSurveysStub } = makeSut()
-    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve([])))
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(Promise.resolve([]))
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(noContent())
   })
